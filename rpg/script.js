018f635591e70067cceaecfa64d9e83b522456cf
choilin310 <choiinhwan0@gmail.com>
2024-01-10 18:41:18 -0500

    goFight function made

--- rpg/script.js
+++ rpg/script.js
@@ -53,6 +53,12 @@ const locations = [
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters.",
   },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
+  }
 ];
 
 // initialize buttons
@@ -142,6 +148,10 @@ function fightBeast() {
 function fightDragon() {
   fighting = 2;
   goFight();
+}
+
+function goFight() {
+  update(locations[3])
 }
 
 function attack() {
