c198bbc8dfbc16d1342becee33bd56d8177c9619
choilin310 <choiinhwan0@gmail.com>
2024-01-10 21:02:16 -0500

    finished attack function and made lose and defeatMonster function

--- rpg/script.js
+++ rpg/script.js
@@ -151,11 +151,15 @@ function fightDragon() {
 }
 
 function goFight() {
-  update(locations[3])
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 function attack() {
-
+  text.innerText = "The" + monsters[fighting].name + " attacks."
 }
 
 function dodge(){}
