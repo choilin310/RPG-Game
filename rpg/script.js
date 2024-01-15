a9ac0cd9bd2afbe4d344fdc320335f3599cabfa9
choilin310 <choiinhwan0@gmail.com>
2024-01-15 17:36:48 -0500

    added kill monster on locations

--- rpg/script.js
+++ rpg/script.js
@@ -176,6 +176,10 @@ function lose(){}
 
 function defeatMonster() {
   gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
 }
 
 function dodge(){
